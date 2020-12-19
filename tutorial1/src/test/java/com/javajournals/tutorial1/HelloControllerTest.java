ef112c210f4f886be0f18b0e59112f28284ba9ac
javajournals <pankaj.desai@javajournals.com>
2020-12-19 15:13:25 -0500

    Toutrial-1 : springboot Getting Started

---
+++ tutorial1/src/test/java/com/javajournals/tutorial1/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.javajournals.tutorial1;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.javajournals.tutorial1.controller.HelloWorldController;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class HelloControllerTest {
+	
+	private MockMvc mvc;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+
+		mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().string(equalTo("Hello World")));
+	}
+}
